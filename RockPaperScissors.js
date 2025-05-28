d24d1becc40bfd72f6f3200dec16ce2060775c0b
samanthaahannah <samanthaahannah@outlook.com>
2025-05-28 16:56:13 +0100

    Add DOM elemeents to logic

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,129 +1,123 @@
+const humanMsg = document.createElement('div');
+const computerMsg = document.createElement('div');
+const roundMsg = document.createElement('div');
 
-/*function getComputerChoice(){
+let choices = document.querySelector('.container');
+
+function getComputerChoice(){
     let computerNumber = Math.random();
     let computerChoice = "";
 
      if (computerNumber < 0.333){
         computerChoice = "rock";
-        alert("Computer picked rock!");
+        computerMsg.textContent = "Computer picked rock!";
+        choices.appendChild(computerMsg);
         return computerChoice;
     }
     else if (computerNumber >= 0.333 && computerNumber < 0.666){
         computerChoice = "paper";
-        alert("Computer picked paper!");
+        computerMsg.textContent = "Computer picked paper!";
+        choices.appendChild(computerMsg);
         return computerChoice;
     }
     else if (computerNumber > 0.666){
         computerChoice = "scissors";
-        alert("Computer picked scissors!");
+        computerMsg.textContent = "Computer picked scissors!";
+        choices.appendChild(computerMsg);
         return computerChoice;
     }
-}*/
-
-const msg = document.createElement('div');
-
-let humanChoices = document.querySelector('.container');
-
-humanChoices.addEventListener('click', (event) => {
-
-    let target = event.target;
-
-    switch(target.id){
-        case 'rock':
-            msg.textContent = "You picked rock!";
-            humanChoices.appendChild(msg);
-            break;
-        case 'paper':
-            msg.textContent = "You picked paper!";
-            humanChoices.appendChild(msg);
-            break;
-        case 'scissors':
-            msg.textContent = "You picked scissors!";
-            humanChoices.appendChild(msg);
-            break;
-        default:
-    }
-});
-
-
-
-/*function getHumanChoice(){
-    let humanChoice = prompt("Play either Rock, Paper or Scisors: ");
-
-    humanChoice = humanChoice.toLowerCase();
-
-    if (humanChoice == "rock"){
-        alert("You picked rock!");
-        return humanChoice;
-    }
-    else if (humanChoice == "paper"){
-        alert("You picked paper!");
-        return humanChoice;
-    }
-    else if (humanChoice == "scissors"){
-        alert("You picked scissors!");
-        return humanChoice;
-    }
-    else{
-        alert("Invalid entry. Try again");
-        getHumanChoice();
-    }
-    
 }
+
 
 function playRound(humanChoice, computerChoice){
     switch (humanChoice){
     case "rock":
         if (computerChoice == "rock"){
-            alert("It's a draw!");
+            roundMsg.textContent = "It's a draw!";
+            choices.appendChild(roundMsg);
         }
         else if (computerChoice == "scissors"){
-            alert("Rock beats scissors! You win this round!");
+            roundMsg.textContent = "Rock beats scissors! You win this round!";
+            choices.appendChild(roundMsg);
             return humanScore += 1;
         }
         else if (computerChoice == "paper"){
-            alert("Paper beats rock! You lose this round");   
+            roundMsg.textContent = "Paper beats rock! You lose this round";
+            choices.appendChild(roundMsg);
             return  computerScore += 1;
         }
         break;
     case "scissors":
         if (computerChoice == "rock"){
-            alert("Rock beats scissors! You lose this round!"); 
+            roundMsg.textContent = "Rock beats scissors! You lose this round!";
+            choices.appendChild(roundMsg); 
             return  computerScore += 1;
         }
         else if (computerChoice == "scissors"){
-            alert("It's a draw!");
+            roundMsg.textContent = "It's a draw!";
+            choices.appendChild(roundMsg); 
         }
         else if (computerChoice == "paper"){
-            alert("Scissors beats paper! You win this round!");
+            roundMsg.textContent = "Scissors beats paper! You win this round!";
+            choices.appendChild(roundMsg); 
             return humanScore += 1;
         }
         break;
     case "paper":
         if (computerChoice == "rock"){
-            alert("Paper beats rock! You win this round");
+            roundMsg.textContent = "Paper beats rock! You win this round";
+            choices.appendChild(roundMsg); 
             return humanScore += 1;
         }
         else if (computerChoice == "scissors"){
-            alert("Scissors beats paper! You lose this round!");
+            roundMsg.textContent = "Scissors beats paper! You lose this round!";
+            choices.appendChild(roundMsg); 
             return  computerScore += 1;
         }
         else if (computerChoice == "paper"){
-            alert("It's a draw!");
+            roundMsg.textContent = "It's a draw!";
+            choices.appendChild(roundMsg); 
         }
         break;
     }
 }
 
 function playGame(){
+
     
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
+    choices.addEventListener('click', (event) => {
+
+    let target = event.target;
+    let humanChoice = '';
+    let computerSelection = getComputerChoice();
+
+
+    switch(target.id){
+        case 'rock':
+            humanMsg.textContent = "You picked rock!";
+            choices.appendChild(humanMsg);
+            humanChoice = 'rock';
+            playRound(humanChoice, computerSelection);
+            break;
+        case 'paper':
+            humanMsg.textContent = "You picked paper!";
+            choices.appendChild(humanMsg);
+            humanChoice = 'paper';
+            playRound(humanChoice, computerSelection);
+            break;
+        case 'scissors':
+            humanMsg.textContent = "You picked scissors!";
+            choices.appendChild(humanMsg);
+            humanChoice = 'scissors';
+            playRound(humanChoice, computerSelection);
+            break;
+        default:
+    }
+});
     
 }
 
-function scoreState(){
+/*function scoreState(){
     stringWin = "You won the game! Congrats!";
     stringLose = "You lost! Sorry!";
     stringDraw = "It was a draw! Nobody won!";
@@ -139,7 +133,7 @@ function scoreState(){
     else{
         alert(stringDraw);
     }
-}
+}*/
 
 
 
@@ -147,7 +141,6 @@ let humanScore = 0;
 let computerScore = 0;
 
 playGame();
-scoreState();*/
 
 
 
